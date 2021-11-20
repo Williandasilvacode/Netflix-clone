232b68d4ac52d5aa370ebc7ecd0f16552fa60f46
Willian da Silva <williandasilvaoficial@gmail.com>
2021-11-20 00:15:14 -0300

    addparte2

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,16 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    margin:0,
     nav:false,
     responsive:{
         0:{
-            items:1
-        },
-        600:{
             items:3
         },
+        600:{
+            items:6
+        },
         1000:{
-            items:5
+            items:10
         }
     }
 })
